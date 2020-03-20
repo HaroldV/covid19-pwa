7bc7a58a30a1b9d8a47aeafe76e1bbe58d6c9136
Javier Gómez <javiergomezve@gmail.com>
2020-03-20 10:36:03 -0500

    [add] new card's design

--- src/components/List/ListItem.js
+++ src/components/List/ListItem.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import moment from 'moment';
-import { Bar, BarChart, CartesianGrid, Legend, Tooltip, YAxis } from 'recharts';
+
+import './ListItem.scss';
+import deathsIcon from '../../assets/icons/deaths.png';
+import confirmedIcon from '../../assets/icons/confirmed.png';
+import recoveredIcon from '../../assets/icons/recovered.png';
+
+function numberWithThousands(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+}
 
 const ListItem = ({ item }) => {
-    const d = [
-        {confirmed: item.confirmed,  deaths: item.deaths, recovered: item.recovered}
-    ];
 
     return (
         <div className="item">
-            <BarChart
-                width={360}
-                height={200}
-                data={d}
-                margin={{
-                    top: 5, right: 20, left: 20, bottom: 5,
-                }}
-            >
-                <CartesianGrid strokeDasharray="3 3"/>
-                <YAxis/>
-                <Tooltip/>
-                <Legend/>
-                <Bar dataKey="deaths" fill="#8884d8"/>
-                <Bar dataKey="recovered" fill="#82ca9d"/>
-            </BarChart>
-
-            <div className="last-update">{moment(item.lastUpdate).format('DD/MM/YYYY')}</div>
-
             <div>
                 <span className="country">{item.country}</span>{' '}
                 <span className="province">{item.province}</span>
+            </div>
+
+            <div className="last-update">{moment(item.lastUpdate).format('DD/MM/YYYY')}</div>
+
+            <div className="data-wrapper">
+                <div className="data-item">
+                    <img src={confirmedIcon} alt="Confirmados"/>
+                    <p className="confirmed">{numberWithThousands(item.confirmed)}</p>
+                </div>
+
+                <div className="data-item">
+                    <img src={deathsIcon} alt="Muertes"/>
+                    <p className="deaths">{numberWithThousands(item.deaths)}</p>
+                </div>
+
+                <div className="data-item">
+                    <img src={recoveredIcon} alt="Recuperados"/>
+                    <p className="recovered">{numberWithThousands(item.recovered)}</p>
+                </div>
             </div>
         </div>
     );
